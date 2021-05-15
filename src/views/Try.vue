643cb58fb99f058eb9a2ada69654cb4e10f90f10
Hiyeon Kim <ohyeahbravo@gmail.com>
2021-05-15 23:58:06 +0200

    add getCoordinates function in Try page

--- src/views/Try.vue
+++ src/views/Try.vue
@@ -2,6 +2,21 @@
   <div
     class="w-full py-10 px-32 flex flex-col items-center space-y-10 font-mono"
   >
+    <div class="flex flex-row space-x-3 items-center justify-center">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        class="h-5 w-5"
+        viewBox="0 0 20 20"
+        fill="currentColor"
+      >
+        <path
+          fill-rule="evenodd"
+          d="M5.05 4.05a7 7 0 119.9 9.9L10 18.9l-4.95-4.95a7 7 0 010-9.9zM10 11a2 2 0 100-4 2 2 0 000 4z"
+          clip-rule="evenodd"
+        />
+      </svg>
+      <span> {{ cityName }} </span>
+    </div>
     <div class="flex flex-row space-x-10 items-center justify-center">
       <div class="flex flex-row space-x-3 items-center justify-center">
         <img
@@ -29,7 +44,12 @@
       id="try-box"
       class="w-full transition delay-150 duration-300 ease-in-out border-2 border-gray-700 focus:border-gray-200 focus:ring focus:ring-red-300 rounded leading-relaxed h-96 py-4 px-5"
     ></textarea>
-      <button @click.prevent="openPrintSetting" class="p-3 bg-red-300 font-mono hover:bg-red-400 text-white">Print</button>
+    <button
+      @click.prevent="openPrintSetting"
+      class="p-3 bg-red-300 font-mono hover:bg-red-400 text-white"
+    >
+      Print
+    </button>
   </div>
 </template>
 
@@ -42,8 +62,29 @@ export default {
     const humidity = ref(null);
     const temp = ref(null);
     const wind = ref(null);
+    const cityID = ref(null);
+    const cityName = ref(null);
+
     // TODO: currently set to Weimar, need to dynamically fetch
-    const cityID = 2812482;
+    function getCityfromCoords(coords) {
+      const cityName = "Weimar";
+      const cityID = 2812482;
+      console.log(coords);
+      return [cityName, cityID];
+    }
+
+    function getLocation() {
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition((pos) => {
+          [cityName.value, cityID.value] = getCityfromCoords({
+            lat: pos.coords.latitude,
+            lon: pos.coords.longitude,
+          });
+        });
+      } else {
+        [cityName.value, cityID.value] = ["Weimar", 2812482];
+      }
+    }
 
     // TODO: change the font setting after fetching
     function getWeather(cityID) {
@@ -60,7 +101,7 @@ export default {
         .then(function (data) {
           // console.log(data);
           humidity.value = data.main.humidity;
-          temp.value = Math.round(parseFloat(data.main.temp)-273.15);
+          temp.value = Math.round(parseFloat(data.main.temp) - 273.15);
           wind.value = data.wind.speed;
         })
         .catch(function (error) {
@@ -70,9 +111,10 @@ export default {
     }
 
     function openPrintSetting() {
-      console.log("open the print setting")
+      console.log("open the print setting");
     }
 
+    getLocation();
     getWeather(cityID);
 
     // TODO: change the interval if needed
@@ -81,10 +123,11 @@ export default {
     }, 5000);
 
     return {
+      cityName,
       humidity,
       wind,
       temp,
-      openPrintSetting
+      openPrintSetting,
     };
   },
 };
